7228d44a00ebaf8ab336076fcf3c28484d80c2ac
György Mátyás <gy.matyas97@gmail.com>
2017-06-19 13:50:57 +0200

    Palindrome is Done

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,8 +1,20 @@
-def palindrome(str):
-    return
+def palindrome(string):
+    list1 = []
+    list2 = []
+    for c in string:
+        if not c==" ":
+            list1.append(str.upper(c))
+    for c in reversed(string):
+        if not c==" ":
+            list2.append(str.upper(c))
+    if list1==list2:
+        return True
+    return False
 
 
 def main():
+    x = input("Please enter the text: ")
+    print(palindrome(x))
     return
 
 
