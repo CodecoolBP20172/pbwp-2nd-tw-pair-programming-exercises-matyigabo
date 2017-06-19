7c7152b35004d91910466f9af1622726e7711862
György Mátyás <gy.matyas97@gmail.com>
2017-06-19 13:36:57 +0200

    ListOverLap is Done

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,8 +1,21 @@
 def listoverlap(list1, list2):
-    return
+    c = []
+    for i in list1:
+        if i in list2:
+            if i in c:
+                continue
+            c.append(i)
+    return c
 
-
+import random
 def main():
+    a = [5]
+    b = [5]
+    for i in range(random.randint(5,15)):
+        a.append(random.randint(0,100))
+    for i in range(random.randint(5,15)):
+        b.append(random.randint(0,100))
+    print(listoverlap(a, b))
     return
 
 
