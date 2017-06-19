0f737c89a64d9138272e0653d1b9d768b2480eed
György Mátyás <gy.matyas97@gmail.com>
2017-06-19 15:00:18 +0200

    Years module is Done

--- years/years_module.py
+++ years/years_module.py
@@ -2,11 +2,17 @@ import datetime
 
 
 def years(age):
-    return
+    result=int((100-age+2017))
+    return result
 
 
 def main():
-    return
+    name = input("Please enter your name: ")
+    age = int(input("Please enter your age: "))
+    times = int(input("How many times you want to see it?: "))
+    for i in range(times):    
+        print("Dear %s! You'll be 100 year old in %d" % (name,years(age)),end='\n')
+    
 
 
 if __name__ == '__main__':
