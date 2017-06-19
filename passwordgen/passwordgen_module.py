7ab8135cdc6ed00961e3e55d1cfee6547f297238
György Mátyás <gy.matyas97@gmail.com>
2017-06-19 14:42:46 +0200

    PasswordGen is Done

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,9 +1,33 @@
+import string
+import random
 def passwordgen():
-    return
-
-
+    list_lowercase = []
+    list_uppercase = []
+    list_syms = ["!","@","#","$","%","^","&","*","(",")","?"] # !@#$%^&*()?
+    password = []
+    length = 0
+    while length < 8:
+        length = int(input("Please enter the length of your new password (min. 8): "))
+    for i in string.ascii_lowercase:
+        list_lowercase.append(i)
+    for i in string.ascii_uppercase:
+        list_uppercase.append(i)
+    for x in range(length):
+        y = random.randint(1,4)
+        if y == 1:
+            password.append(list_lowercase[random.randint(0,len(list_lowercase)-1)])
+        if y == 2:
+            password.append(list_uppercase[random.randint(0,len(list_uppercase)-1)])
+        if y == 3:
+            password.append(random.randint(0,9))
+        if y == 4:
+            password.append(list_syms[random.randint(0,len(list_syms)-1)])
+    return(''.join(map(str,password)))
+  
 def main():
-    return
+    passwordgen()
+    
+    
 
 
 if __name__ == '__main__':
